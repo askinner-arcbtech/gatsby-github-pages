03ae953c5d90996f9d131ac9e9be89f167521de0
Antonio Skinner <askinner@arcbtech.com>
2022-06-11 17:41:59 -0500

    first push of public info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,13 @@
+/**
+ * Configure your Gatsby site with this file.
+ * 
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
 module.exports = {
-  siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
-  },
+  pathPrefix: "/gatsby-github-pages",
+  //siteMetadata: {
+  //  siteUrl: `https://www.yourdomain.tld`,
+  //},
   plugins: [],
 }
